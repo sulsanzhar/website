bea7b640f6cf61a853be72c8f4347a67925ac083
Sultanov Sanzhar <sulsanzhar@mail.ru>
2024-04-13 22:03:23 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src="./src/img/itachi.jpeg" style={{width: '50%', height: '50%',  borderRadius: '50%'}} alt="" />
+      <h1>Salam</h1>
+    </>
+  )
+}
+
+export default App
