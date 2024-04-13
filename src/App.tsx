2c6766dc102068d2de9a68077574b9defc24bce0
Sultanov Sanzhar <sulsanzhar@mail.ru>
2024-04-13 22:11:21 +0500

    fourth  commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import Itachi from './img/itachi.jpeg'
 
 function App() {
   return (
     <>
-      <img src="./src/img/itachi.jpeg" style={{width: '50%', height: '50%',  borderRadius: '50%'}} alt="" />
+      <img src={ Itachi } style={{width: '50%', height: '50%',  borderRadius: '50%'}} alt="itachi" />
       <h1>Salam</h1>
     </>
   )
